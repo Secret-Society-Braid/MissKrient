388b6b8ad79dcceed036493335a34840afaa5e1e
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-09-13 15:59:05 +0900

    Main component add

--- src/main/java/org/braid/society/secret/misskrient/MisskrientApplication.java
+++ src/main/java/org/braid/society/secret/misskrient/MisskrientApplication.java
@@ -1,6 +1,7 @@
 package org.braid.society.secret.misskrient;
 
 import java.io.IOException;
+import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -16,7 +17,9 @@ public class MisskrientApplication extends Application {
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(
       MisskrientApplication.class.getResource("main-view.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 960, 640);
+    Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+    scene.getStylesheets().add(
+      Objects.requireNonNull(MisskrientApplication.class.getResource("main.css")).toExternalForm());
     stage.setTitle("Hello!");
     stage.setScene(scene);
     stage.show();
