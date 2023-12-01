3544517c962fd47ec9dff789006e9520c9fa8bc6
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-01 15:48:49 +0900

    fix constructor visibility issue where AbstractMfmNode constructor was set to PACKAGE

--- src/main/java/org/braid/society/secret/misskrient/internal/mfm/AbstractMfmNode.java
+++ src/main/java/org/braid/society/secret/misskrient/internal/mfm/AbstractMfmNode.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import org.braid.society.secret.misskrient.api.mfm.MfmNode;
 
-@NoArgsConstructor(access = AccessLevel.PACKAGE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class AbstractMfmNode implements MfmNode {
 
   protected List<MfmNode> children;
