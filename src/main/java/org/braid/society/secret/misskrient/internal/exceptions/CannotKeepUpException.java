62bdc5ac87bc416ccbdbb472eb58f5f253a574c1
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-01 14:27:22 +0900

    delete class file

--- src/main/java/org/braid/society/secret/misskrient/internal/exceptions/CannotKeepUpException.java
+++
@@ -1,8 +0,0 @@
-package org.braid.society.secret.misskrient.internal.exceptions;
-
-public class CannotKeepUpException extends RuntimeException {
-
-  public CannotKeepUpException() {
-    super("Cannot keep up! The app will be terminated!");
-  }
-}
