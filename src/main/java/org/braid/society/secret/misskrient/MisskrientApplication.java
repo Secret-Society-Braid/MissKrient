df6e17d8a9042cbc105b25326f92e3aca37aecd8
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-27 14:39:52 +0900

    Make abstract gui roughly (#30)
    * add necessary file
    
    * add fxml styles
    
    * Main component add
    
    * Add abstract GUI structure
    
    * fix window size
    
    * make some changes for GUI
    
    * wrap in scrollpane

--- src/main/java/org/braid/society/secret/misskrient/MisskrientApplication.java
+++ src/main/java/org/braid/society/secret/misskrient/MisskrientApplication.java
@@ -1,6 +1,7 @@
 package org.braid.society.secret.misskrient;
 
 import java.io.IOException;
+import java.util.Objects;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -15,8 +16,10 @@ public class MisskrientApplication extends Application {
   @Override
   public void start(Stage stage) throws IOException {
     FXMLLoader fxmlLoader = new FXMLLoader(
-      MisskrientApplication.class.getResource("hello-view.fxml"));
-    Scene scene = new Scene(fxmlLoader.load(), 960, 640);
+      MisskrientApplication.class.getResource("main-view.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+    scene.getStylesheets().add(
+      Objects.requireNonNull(MisskrientApplication.class.getResource("main.css")).toExternalForm());
     stage.setTitle("Hello!");
     stage.setScene(scene);
     stage.show();
--- src/main/java/org/braid/society/secret/misskrient/controller/MisskrientController.java
+++ src/main/java/org/braid/society/secret/misskrient/controller/MisskrientController.java
@@ -1,83 +1,37 @@
 package org.braid.society.secret.misskrient.controller;
 
-import java.awt.Desktop;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javax.swing.JOptionPane;
+import javafx.scene.control.Button;
 import lombok.extern.slf4j.Slf4j;
-import misskey4j.Misskey;
-import misskey4j.MisskeyFactory;
-import misskey4j.api.request.notes.NotesTimelineRequest;
-import misskey4j.api.response.notes.NotesTimelineResponse;
-import misskey4j.entity.contant.Scope;
-import misskey4j.entity.share.Response;
-import org.braid.society.secret.misskrient.api.account.AccountInfo;
-import org.braid.society.secret.misskrient.api.auth.Auth;
-import org.braid.society.secret.misskrient.api.auth.AuthSelector;
-import org.braid.society.secret.misskrient.api.auth.AuthUrlConfig;
-import org.braid.society.secret.misskrient.api.builder.MiAuthUrlConfigBuilder;
-import org.braid.society.secret.misskrient.api.database.Repository;
-import org.braid.society.secret.misskrient.api.database.RepositorySelector;
 
 @Slf4j
 public class MisskrientController {
 
   @FXML
-  private Label welcomeText;
+  protected Button funcButton;
 
   @FXML
-  protected void onHelloButtonClick() {
-    log.info("onHelloButtonClick");
-    welcomeText.setText("Welcome to JavaFX Application!");
+  protected void onColumnSwitchButtonPressed(ActionEvent e) {
+    log.info("onColumnSwitchButtonPressed");
+    log.info("Event detail: {}", e);
+    log.info("Event source: {}", e.getSource());
 
-    Repository<AccountInfo> r = RepositorySelector.forAccount();
-    AccountInfo i = r.get(0);
-
-    Misskey m = MisskeyFactory.getInstanceWithOwnedAccessToken(i.getHost(), i.getAccessToken());
-    Response<NotesTimelineResponse[]> res = m
-      .notes()
-      .timeline(
-        NotesTimelineRequest.builder()
-          .limit(100L)
-          .includeMyRenotes(false)
-          .includeLocalRenotes(false)
-          .build());
-    for (NotesTimelineResponse response : res.get()) {
-      log.info(response.toString());
+    if (e.getSource() instanceof Button b) {
+      log.info("Button text: {}", b.getText());
+    } else {
+      throw new ClassCastException(
+        "Failed to recognize event source due to improper action event property.");
     }
+
+    // TODO: implement column switching by button text
   }
 
   @FXML
-  protected void onAuthButtonClick() {
-    log.info("Start auth");
-    AuthUrlConfig config = MiAuthUrlConfigBuilder.newBuilder()
-      .setHostname("misskey.io")
-      .setScopes(Scope.ALL)
-      .build();
-    Repository<AccountInfo> r = RepositorySelector.forAccount();
-    Auth auth = AuthSelector.forMisskey(config, r);
-    try {
-      Desktop
-        .getDesktop()
-        .browse(new URL(auth.constructAuthUrl()).toURI());
-    } catch (IOException | URISyntaxException e) {
-      log.error("Failed to process URLs", e);
-    }
-    JOptionPane.showConfirmDialog(
-      null,
-      "Please press OK after authentication.",
-      "Misskrient",
-      JOptionPane.OK_CANCEL_OPTION);
-
-    auth.authenticate();
-
-    JOptionPane.showConfirmDialog(
-      null,
-      "Authentication completed.",
-      "Misskrient",
-      JOptionPane.OK_CANCEL_OPTION);
+  protected void onFuncButtonPressed(ActionEvent e) {
+    log.info("onFuncButtonPressed");
+    log.info("Event detail: {}", e);
+    log.info("Event source: {}", e.getSource());
   }
+
 }
