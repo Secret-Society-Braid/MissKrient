4dba218c745224df2af859c66f0f0a14097d07ec
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-27 18:16:47 +0900

    add ability to get instance for misskey manipulation

--- src/main/java/org/braid/society/secret/misskrient/api/account/AccountInfo.java
+++ src/main/java/org/braid/society/secret/misskrient/api/account/AccountInfo.java
@@ -1,6 +1,8 @@
 package org.braid.society.secret.misskrient.api.account;
 
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import misskey4j.Misskey;
+import misskey4j.MisskeyFactory;
 import org.braid.society.secret.misskrient.internal.account.AccountInfoImpl;
 
 @JsonDeserialize(as = AccountInfoImpl.class)
@@ -11,4 +13,8 @@ public interface AccountInfo {
   String getAccessToken();
 
   String getUserName();
+
+  default Misskey getInstance() {
+    return MisskeyFactory.getInstanceWithOwnedAccessToken(this.getHost(), this.getAccessToken());
+  }
 }
