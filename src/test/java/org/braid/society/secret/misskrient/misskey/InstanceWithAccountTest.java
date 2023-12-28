99da00e15f88e622324529fa3cd1dd144f97e0ff
hizumiaoba <56146205+hizumiaoba@users.noreply.github.com>
2023-12-28 11:40:43 +0900

    add test for create instance of misskey client

---
+++ src/test/java/org/braid/society/secret/misskrient/misskey/InstanceWithAccountTest.java
@@ -0,0 +1,26 @@
+package org.braid.society.secret.misskrient.misskey;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import misskey4j.Misskey;
+import misskey4j.api.request.i.IRequest;
+import misskey4j.api.response.i.IResponse;
+import org.braid.society.secret.misskrient.api.account.AccountInfo;
+import org.braid.society.secret.misskrient.api.database.RepositorySelector;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class InstanceWithAccountTest {
+
+  @Test
+  void fromAccountTest() {
+    AccountInfo i = RepositorySelector.forAccount().get(0);
+    Misskey m = i.getInstance();
+    IResponse r = m.accounts().i(IRequest.builder().build()).get();
+
+    assertThat(r.getUsername()).isEqualTo(i.getUserName());
+    assertThat(m.getHost()).contains(i.getHost());
+  }
+}
